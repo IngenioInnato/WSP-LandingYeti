5feb7365a437ff965a9f0ff694f8946c97373d8b
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-06-18 13:14:52 -0400

    Actualizando cambios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,44 @@
 const app = new Vue({
   el: '#app',
-  data: {},
-  methods: {},
+  data: {
+    form:{
+      name: 'Nombre',
+      phone: 'Teléfono',
+      email: 'Email'
+    }
+  },
+  methods: {
+    getDataForm(){
+      // e.preventDefault;
+      var formulario = document.querySelectorAll('.contacto');
+      formulario.forEach(el => {
+        el.innerHTML = /*html*/`
+          <div class="bg-light_blue p-5 text-center">
+            <h2 class="text-white">¡Gracias por tu Reserva!</h2>
+            <p class="text-white">En breves momentos te contactaremos</p>
+          </div>
+        `;
+      });
+      console.log(this.form.name + " | " + this.form.phone + " | " + this.form.email)
+    },
+    sendData(data){
+      // 5e7262f5-9fef-45ea-aeab-6a2c91c84681 
+      Email.send({
+        SecureToken : "5e7262f5-9fef-45ea-aeab-6a2c91c84681 ",
+        To : 'worldsolarpropr@gmail.com',
+        From : "Info World Solar Pro",
+        Subject : "Nueva Reservación Yeti",
+        Body : `
+        <h1>Datos dados por el usuario</h1>
+        <p>Nombre: ${data.name}. <br>
+        Teléfono: ${data.phone}. <br>
+        Email: ${data.email}.
+        </p>
+        `
+    }).then(
+      message => alert(message)
+    );
+    }
+  },
   computed: {}
 })
